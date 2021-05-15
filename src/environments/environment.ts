bd365bad3267cc5bb9daaa1dca93dcc1c6c3db54
YATACOBLAS <juan_blas_1905@hotmail.com>
2021-05-15 17:47:35 -0500

    FindMe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-   firebaseConfig:{
-    apiKey: "AIzaSyA-mW26N9okI5NP-VLeWK_r6TKrpQ_8vgc",
-    authDomain: "omega-scope-283806.firebaseapp.com",
-    projectId: "omega-scope-283806",
-    storageBucket: "omega-scope-283806.appspot.com",
-    messagingSenderId: "256779455956",
-    appId: "1:256779455956:web:73158147962a05c50c9a41"
+  firebaseConfig:{
+    apiKey: "--",
+    authDomain: "--",
+    projectId: "--",
+    storageBucket: "--",
+    messagingSenderId: "--",
+    appId: "--"
   },
-  key:'AIzaSyC7llAR9lRjVud76kKQRVYH1FocrtwZ9bM'
+  key:"KEY GOOGLE CLOUD"
 };
 
 /*
